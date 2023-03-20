a449ac5882d308b2547bb601371bd173cd3f865c
dmc1212 <127051393+Cats12128@users.noreply.github.com>
2023-03-20 20:59:49 +0000

    -- created header on layout.html that puts header on every page if using jinga extend layout command -- linked buttons on header to appropriate pages -- made title change with variable set in python and also printed as part of layout -- changed icon to a sun (still not sure how it works but it looks nice) --made multiple html docs

--- app.py
+++ app.py
@@ -3,9 +3,21 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def MainPage():
-  return render_template("home.html")
-  return "Testing"
+def index():
+  return render_template("index.html",addtitle="Home")
 
-if __name__ == "__main__":
-  app.run(host="0.0.0.0", debug=True)
+@app.route("/gallery")
+def gallery():
+  return render_template("gallery.html", addtitle="Gallery")
+
+@app.route("/about")
+def about():
+  return render_template("about.html", addtitle="About")
+
+@app.route("/contact")
+def contact():
+  return render_template("contact.html", addtitle="Contact")
+
+
+#if __name__ == "__main__":
+app.run(host="0.0.0.0", debug=True)
